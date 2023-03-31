a3fbc174a05afb0fdfbcfd7408d3ac4cd8e73e1f
Chinna143dharani <129508237+Chinna143dharani@users.noreply.github.com>
2023-03-31 16:53:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import  streamlit
 streamlit.title('my parents new healthy dinner')
+streamlit.header('BREAKFAST MENU')
+streamlit.text('idly')
+streamlit.text('dosa with coconut chutny')
+streamlit.text('chapathi with alu curry'')
